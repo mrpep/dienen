29de5e1f5e25551fdecf242ecd1a2302b31fb075
mrpep <leonardodpepino@gmail.com>
2021-04-21 23:04:07 +0000

    add comments to test

--- tests/mnist_full_example.py
+++ tests/mnist_full_example.py
@@ -1,13 +1,18 @@
+#IMPORTS
+
 import tensorflow as tf
 import dienen
 import pickle
 import numpy as np
 
+#Get the MNIST dataset using tf utilities:
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data(path='mnist.npz')
 
+#Feature scaling:
 x_train = x_train/255.
 x_test = x_test/255.
 
+#Use the first 2000 instances for validation:
 x_val = x_train[:2000]
 y_val = y_train[:2000]
 
@@ -16,19 +21,19 @@ y_train = y_train[2000:]
 
 results = {}
 for model_name, model_config in dict(mlp='mlp.yaml', cnn='cnn.yaml').items():
-    dnn = dienen.Model(model_config)
-    dnn.set_data((x_train,y_train),validation = (x_val,y_val))
-    keras_model = dnn.build()
+    dnn = dienen.Model(model_config) #Load the model
+    dnn.set_data((x_train,y_train),validation = (x_val,y_val)) #Set the dataset
+    keras_model = dnn.build() #Build the model
 
-    dnn.fit()
+    dnn.fit() #Train the model
 
-    pickle.dump(dnn,open('{}.model'.format(model_name),'wb'))
-    dnn = pickle.load(open('{}.model'.format(model_name),'rb'))
+    pickle.dump(dnn,open('{}.model'.format(model_name),'wb')) #Save it (dienen models are serializable)
+    dnn = pickle.load(open('{}.model'.format(model_name),'rb')) #Load it
 
-    y_probs = dnn.predict(x_test)
+    y_probs = dnn.predict(x_test) #Make some predictions
     y_pred = np.argmax(y_probs,axis=1)
 
-    results[model_name] = 100*(y_pred == y_test).sum()/len(y_pred)
+    results[model_name] = 100*(y_pred == y_test).sum()/len(y_pred) #Calculate accuracy
 
 for model_name, model_acc in results.items():
     print('{} model Test accuracy: {:.2f}%'.format(model_name,model_acc))
